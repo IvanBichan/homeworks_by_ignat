1e7229736f3b40617e4ad552433f0ed43d389811
IvanBichan <ivanbichanfe@gmail.com>
2022-11-07 22:57:27 +0300

    feat: make working HW1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import style from "./Message.module.css"
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props:MessagePropsType) {
     return (
-        <div>
+        <div className={style.message}>
+            <div className={style.avatar}>
+                <img src={props.avatar}/>
+            </div>
+            <div className={style.messageConteiner}>
+                <div>{props.name}</div>
+                <div className={style.messageValue}>
+                    <div>{props.message}</div>
+                    <div>{props.time}</div>
+                </div>
+            </div>
+
 
         </div>
     )
